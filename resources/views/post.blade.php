6e0ab71333c6fb291282046bbdb50d816889b5ed
farahzuot <farahz997@eng.hu.edu.jo>
2021-10-01 01:30:19 +0300

    .

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<head>
+    <title>blog app</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+<article>
+ <?= $post ?>
+</article>
+
+<a href="/">Home</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+
+<head>
+    <title>blog app</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">first post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at aut deserunt, eaque fugit molestiae nostrum quidem quo rerum voluptates! Dicta distinctio ducimus id minus mollitia numquam pariatur perferendis. Fugit.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/posts/my-second-post">sec post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at aut deserunt, eaque fugit molestiae nostrum quidem quo rerum voluptates! Dicta distinctio ducimus id minus mollitia numquam pariatur perferendis. Fugit.
+    </p>
+</article>
+<article>
+    <h1><a href="/posts/my-third-post">third post</a></h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at aut deserunt, eaque fugit molestiae nostrum quidem quo rerum voluptates! Dicta distinctio ducimus id minus mollitia numquam pariatur perferendis. Fugit.
+    </p>
+</article>
+</body>
