6bd366744ad521e488a7d378130524794a43ba75
farahzuot <70090888+farahzuot@users.noreply.github.com>
2021-11-07 22:36:26 +0200

    .

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @foreach ($posts as $post)
         <article class="{{ $loop -> even ? 'even' : 'odd' }}">
             <a href="/posts/{{$post -> slug}} "><h1> {{$post -> title}} </h1></a>
-            <p> {{$post -> body}} </p>
+            <p> {!! $post -> body !!} </p>
         </article>
     @endforeach
 </x-layout>
